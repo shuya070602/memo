e6f47c3204d31d652a255860b0475d11bb7e8e82
shuya0070602 <shuya.070602@gmail.com>
2020-05-06 16:57:01 +0900

    新規登録、ログインの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  #デバイス機能実行前にconfigure_permitted_parametersの実行をする。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  #sign_upの際にnameのデータ操作を許。追加したカラム。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
+
 end
