cc71ff29e072963e791f91e2c6f6fb71d21a3eca
shuya0070602 <shuya.070602@gmail.com>
2020-05-06 22:30:08 +0900

    マイページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,27 @@
 class UsersController < ApplicationController
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  	  redirect_to user_path(@user.id)
+  	else
+  	  render "edit"
+  	end
   end
 
   def confirm
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email)
   end
 end
